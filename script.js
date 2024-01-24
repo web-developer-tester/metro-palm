f5c3420b318c48ec6e6b718ff33d6015558f320a
bandana-das-dev <bandana.das@argusnews.in>
2024-01-24 17:23:52 +0530

    gffs

--- script.js
+++ script.js
@@ -26,21 +26,6 @@ var otvshows= $('.section-shows').owlCarousel({
     }
 });
 
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
 // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
@@ -130,6 +115,159 @@ gsap.to("#page2>h1>span",{
 
 
 
+function canvas(){
+    const canvas = document.querySelector("#page3>canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+
+function files(index) {
+  var data = `
+  ./frames00007.png
+  ./frames00010.png
+  ./frames00013.png
+  ./frames00016.png
+  ./frames00019.png
+  ./frames00022.png
+  ./frames00025.png
+  ./frames00028.png
+  ./frames00031.png
+  ./frames00034.png
+  ./frames00037.png
+  ./frames00040.png
+  ./frames00043.png
+  ./frames00046.png
+  ./frames00049.png
+  ./frames00052.png
+  ./frames00055.png
+  ./frames00058.png
+  ./frames00061.png
+  ./frames00064.png
+  ./frames00067.png
+  ./frames00070.png
+  ./frames00073.png
+  ./frames00076.png
+  ./frames00079.png
+  ./frames00082.png
+  ./frames00085.png
+  ./frames00088.png
+  ./frames00091.png
+  ./frames00094.png
+  ./frames00097.png
+  ./frames00100.png
+  ./frames00103.png
+  ./frames00106.png
+  ./frames00109.png
+  ./frames00112.png
+  ./frames00115.png
+  ./frames00118.png
+  ./frames00121.png
+  ./frames00124.png
+  ./frames00127.png
+  ./frames00130.png
+  ./frames00133.png
+  ./frames00136.png
+  ./frames00139.png
+  ./frames00142.png
+  ./frames00145.png
+  ./frames00148.png
+  ./frames00151.png
+  ./frames00154.png
+  ./frames00157.png
+  ./frames00160.png
+  ./frames00163.png
+  ./frames00166.png
+  ./frames00169.png
+  ./frames00172.png
+  ./frames00175.png
+  ./frames00178.png
+  ./frames00181.png
+  ./frames00184.png
+  ./frames00187.png
+  ./frames00190.png
+  ./frames00193.png
+  ./frames00196.png
+  ./frames00199.png
+  ./frames00202.png
+ `;
+  return data.split("\n")[index];
+}
+
+const frameCount = 67;
+
+const images = [];
+const imageSeq = {
+  frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = files(i);
+  images.push(img);
+}
+
+gsap.to(imageSeq, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: `none`,
+  scrollTrigger: {
+    scrub: .5,
+    trigger: `#page3`,
+    start: `top top`,
+    end: `250% top`,
+    scroller: `#main`,
+  },
+  onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+  scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+ScrollTrigger.create({
+
+  trigger: "#page3",
+  pin: true,
+  scroller: `#main`,
+  start: `top top`,
+  end: `250% top`,
+});
+}
+canvas()
+
+
+
+
 
 
 var clutter = "";
@@ -177,6 +315,189 @@ gsap.to("#page6>h1>span",{
 
 
 
+
+
+function canvas2(){
+  const canvas = document.querySelector("#page7>canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+render();
+});
+
+function files(index) {
+var data = `
+
+./1_00114.png
+./1_00115.png
+./1_00116.png
+./1_00117.png
+./1_00118.png
+./1_00119.png
+./1_00120.png
+./1_00121.png
+./1_00122.png
+./1_00123.png
+./1_00124.png
+./1_00125.png
+./1_00126.png
+./1_00127.png
+./1_00105.png
+./1_00106.png
+./1_00107.png
+./1_00108.png
+./1_00109.png
+./1_00110.png
+./1_00111.png
+./1_00112.png
+./1_00113.png
+./1_00113.png
+./1_00099.png
+./1_00100.png
+./1_00101.png
+./1_00102.png
+./1_00103.png
+./1_00104.png
+./1_00093.png
+./1_00094.png
+./1_00095.png
+./1_00096.png
+./1_00097.png
+./1_00086.png
+./1_00087.png
+./1_00088.png
+./1_00089.png
+./1_00090.png
+./1_00091.png
+./1_00092.png
+./1_00079.png
+./1_00080.png
+./1_00081.png
+./1_00082.png
+./1_00083.png
+./1_00084.png
+./1_00085.png
+./1_00073.png
+./1_00074.png
+./1_00075.png
+./1_00076.png
+./1_00077.png
+./1_00078.png
+./1_00066.png
+./1_00067.png
+./1_00068.png
+./1_00069.png
+./1_00070.png
+./1_00071.png
+./1_00072.png
+./1_00059.png
+./1_00060.png
+./1_00061.png
+./1_00062.png
+./1_00063.png
+./1_00064.png
+./1_00065.png
+./1_00050.png
+./1_00051.png
+./1_00052.png
+./1_00053.png
+./1_00054.png
+./1_00055.png
+./1_00056.png
+./1_00057.png
+./1_00058.png
+./1_00041.png
+./1_00042.png
+./1_00043.png
+./1_00044.png
+./1_00045.png
+./1_00046.png
+./1_00047.png
+./1_00048.png
+./1_00049.png
+./1_00031.png
+./1_00032.png
+./1_00033.png
+./1_00034.png
+./1_00035.png
+./1_00036.png
+./1_00037.png
+./1_00038.png
+./1_00039.png
+./1_00040.png
+`;
+return data.split("\n")[index];
+}
+
+const frameCount = 97;
+
+const images = [];
+const imageSeq = {
+frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+const img = new Image();
+img.src = files(i);
+images.push(img);
+}
+
+gsap.to(imageSeq, {
+frame: frameCount - 1,
+snap: "frame",
+ease: `none`,
+scrollTrigger: {
+  scrub: .5,
+  trigger: `#page7`,
+  start: `top top`,
+  end: `250% top`,
+  scroller: `#main`,
+},
+onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+var canvas = ctx.canvas;
+var hRatio = canvas.width / img.width;
+var vRatio = canvas.height / img.height;
+var ratio = Math.max(hRatio, vRatio);
+var centerShift_x = (canvas.width - img.width * ratio) / 2;
+var centerShift_y = (canvas.height - img.height * ratio) / 2;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(
+  img,
+  0,
+  0,
+  img.width,
+  img.height,
+  centerShift_x,
+  centerShift_y,
+  img.width * ratio,
+  img.height * ratio
+);
+}
+ScrollTrigger.create({
+
+trigger: "#page7",
+pin: true,
+scroller: `#main`,
+start: `top top`,
+end: `250% top`,
+});
+}
+canvas2()
 
 
 
