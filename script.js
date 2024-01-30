036f96eaf091ee34d97f30e17472b764fc2a5bea
bandana-das-dev <bandana.das@argusnews.in>
2024-01-30 21:34:39 +0530

    home

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
-var otvshows= $('.section-shows').owlCarousel({
+var otvshows= $('.section-otv-shows').owlCarousel({
     loop:true,
     margin:10,
     dots: false,
     autoHeight : true,
-    autoplayTimeout:2000,
+    autoplayTimeout:5000,
     responsiveClass:true,
     autoplayHoverPause:true,
-    nav:false,
+    nav:true,
+    lazyLoad:true,
     responsive:{
         0:{
             items:1,
             loop:true,
-            autoplay:false,
+            autoplay:true,
         },
         600:{
             items:4,
             loop:true,
-            autoplay:false,
+            autoplay:true,
         },
         1000:{
             items:4,
             loop:true,
-            autoplay:false,
+            autoplay:true,
         }
     }
 });
-
-// Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 24,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            992:{
-                items:2
-            }
-        }
-    });
-    
-
-
 
 $(function(){
   $('.nav-toggle, nav a').on('click',function(){
@@ -316,212 +292,4 @@ gsap.to("#page6>h1>span",{
 
 
 
-
-function canvas2(){
-  const canvas = document.querySelector("#page7>canvas");
-const context = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-
-window.addEventListener("resize", function () {
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-render();
-});
-
-function files(index) {
-var data = `
-
-./1_00114.png
-./1_00115.png
-./1_00116.png
-./1_00117.png
-./1_00118.png
-./1_00119.png
-./1_00120.png
-./1_00121.png
-./1_00122.png
-./1_00123.png
-./1_00124.png
-./1_00125.png
-./1_00126.png
-./1_00127.png
-./1_00105.png
-./1_00106.png
-./1_00107.png
-./1_00108.png
-./1_00109.png
-./1_00110.png
-./1_00111.png
-./1_00112.png
-./1_00113.png
-./1_00113.png
-./1_00099.png
-./1_00100.png
-./1_00101.png
-./1_00102.png
-./1_00103.png
-./1_00104.png
-./1_00093.png
-./1_00094.png
-./1_00095.png
-./1_00096.png
-./1_00097.png
-./1_00086.png
-./1_00087.png
-./1_00088.png
-./1_00089.png
-./1_00090.png
-./1_00091.png
-./1_00092.png
-./1_00079.png
-./1_00080.png
-./1_00081.png
-./1_00082.png
-./1_00083.png
-./1_00084.png
-./1_00085.png
-./1_00073.png
-./1_00074.png
-./1_00075.png
-./1_00076.png
-./1_00077.png
-./1_00078.png
-./1_00066.png
-./1_00067.png
-./1_00068.png
-./1_00069.png
-./1_00070.png
-./1_00071.png
-./1_00072.png
-./1_00059.png
-./1_00060.png
-./1_00061.png
-./1_00062.png
-./1_00063.png
-./1_00064.png
-./1_00065.png
-./1_00050.png
-./1_00051.png
-./1_00052.png
-./1_00053.png
-./1_00054.png
-./1_00055.png
-./1_00056.png
-./1_00057.png
-./1_00058.png
-./1_00041.png
-./1_00042.png
-./1_00043.png
-./1_00044.png
-./1_00045.png
-./1_00046.png
-./1_00047.png
-./1_00048.png
-./1_00049.png
-./1_00031.png
-./1_00032.png
-./1_00033.png
-./1_00034.png
-./1_00035.png
-./1_00036.png
-./1_00037.png
-./1_00038.png
-./1_00039.png
-./1_00040.png
-`;
-return data.split("\n")[index];
-}
-
-const frameCount = 97;
-
-const images = [];
-const imageSeq = {
-frame: 1,
-};
-
-for (let i = 0; i < frameCount; i++) {
-const img = new Image();
-img.src = files(i);
-images.push(img);
-}
-
-gsap.to(imageSeq, {
-frame: frameCount - 1,
-snap: "frame",
-ease: `none`,
-scrollTrigger: {
-  scrub: .5,
-  trigger: `#page7`,
-  start: `top top`,
-  end: `250% top`,
-  scroller: `#main`,
-},
-onUpdate: render,
-});
-
-images[1].onload = render;
-
-function render() {
-scaleImage(images[imageSeq.frame], context);
-}
-
-function scaleImage(img, ctx) {
-var canvas = ctx.canvas;
-var hRatio = canvas.width / img.width;
-var vRatio = canvas.height / img.height;
-var ratio = Math.max(hRatio, vRatio);
-var centerShift_x = (canvas.width - img.width * ratio) / 2;
-var centerShift_y = (canvas.height - img.height * ratio) / 2;
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.drawImage(
-  img,
-  0,
-  0,
-  img.width,
-  img.height,
-  centerShift_x,
-  centerShift_y,
-  img.width * ratio,
-  img.height * ratio
-);
-}
-ScrollTrigger.create({
-
-trigger: "#page7",
-pin: true,
-scroller: `#main`,
-start: `top top`,
-end: `250% top`,
-});
-}
-canvas2()
-
-
-
-gsap.to(".page7-cir",{
-  scrollTrigger:{
-    trigger:`.page7-cir`,
-    start:`top center`,
-    end:`bottom top`,
-    scroller:`#main`,
-    scrub:.5
-  },
-  scale:1.5
-})
-
-
-
-gsap.to(".page7-cir-inner",{
-  scrollTrigger:{
-    trigger:`.page7-cir-inner`,
-    start:`top center`,
-    end:`bottom top`,
-    scroller:`#main`,
-    scrub:.5
-  },
-  backgroundColor : `#0a3bce91`,
-})
 
